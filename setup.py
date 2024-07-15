d193f4ef592edf938ceeb896b6e077819dbe578d
Ashish Kumar Singh <ashishsingh.chunar2017@gmail.com>
2024-07-15 13:04:44 +0530

    final code upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Ashish Kumar Singh',
+    author_email= 'ashishsingh.chunar2017@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
